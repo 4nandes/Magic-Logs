ad02da3c48fa320f6dd84a10d77d0c88dc42e04e
Nathan Fernandes <4nandes@gmail.com>
2018-05-28 01:14:10 -0700

    made a dictionary and a way to get the level out of the scraped data

--- flex.py
+++ flex.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-import pandas 
+
 #Gets the information for a person
 #################################
 sauce = urlopen("http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=Diet%20Conk")
@@ -8,10 +8,11 @@ soup = BeautifulSoup(sauce,'lxml')
 #file = open("rsdata.txt","w")
 #file.write(soup.get_text())
 #file.close()
+data = soup.get_text().split("\n")
 
-vals = pandas.(soup.get_text(),header=None,sep=",",encoding="ISO-8859-1")
+book = {'overall':0, 'attack':1, 'defence':2, 'strength':3, 'hitpoints':4, 'ranged':5, 'prayer':6, 'magic':7, 'cooking':8, 'woodcutting':9, 'fletching':10, 'fishing':11, 'firemaking':12, 'crafting':13, 'smithing':14, 'mining':15, 'herblore':16, 'agility':17, 'theiving':18, 'slayer':19, 'farming':20, 'runecrafting':21, 'hunter':22, 'construction':23}
 
-for x in range(0,24):
-    print(vals[1][x])
-
+level = data[book['defence']].split(",")
+print(level[0])
+#print(soup.get_text())
 #print(file.read())
