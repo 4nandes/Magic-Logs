c6e6a0ff532a2ca1038cecf840411a6cbc3899d1
Nathan Fernandes <4nandes@gmail.com>
2018-06-24 20:25:11 -0700

    more testing

--- flexbot.py
+++ flexbot.py
@@ -31,7 +31,7 @@ labels = ['Overall','Attack','Defence','Strength','Hitpoints',
         'Ranged','Prayer','Magic','Cooking','Woodcutting',
         'Fletching','Fishing','Firemaking','Crafting',
         'Smithing','Mining','Herblore','Agility',
-        'Theiving', 'Slayer', 'Farming','Runecrafting',
+        'Thieving', 'Slayer', 'Farming','Runecrafting',
         'Hunter', 'Construction']
 
 def searchDefault(author, server):
@@ -72,36 +72,41 @@ async def on_message(message):
         skill = await client.wait_for_message(timeout=15.0, author=message.author)
         skill = skill.content.capitalize()
         #Attempt to build a bar chart and a taunting message, if fail then state that the skill they input does not exist
-        try:
-            lvlCaller = dataCaller[labels.index(skill)].split(",") 
-            lvlRec = dataRec[labels.index(skill)].split(",")
-            if int(lvlCaller[1]) > int(lvlRec[1]):
-                await client.send_message(message.channel, 
-                "You ever show off your lvl.%d in %s just to flex on them %s niggas?\n**Flex Strength:** %d Levels %s XP" 
-                %(int(lvlCaller[1]),skill,unRec,(int(lvlCaller[1]) - int(lvlRec[1])),"{:,}".format((int(lvlCaller[2]) - int(lvlRec[2])))))
-                #Traces out the bar chart
-                trace1 = go.Bar(
-                    x= [unCaller, unRec],
-                    y= [int(lvlCaller[2]),int(lvlRec[2])],
-                    text= ["{:,}".format(int(lvlCaller[2])),"{:,}".format(int(lvlRec[2]))],
-                    textposition = 'auto',
-                    marker=dict(color=['#16a085','#cb4335'])
-                )
-                #Layout for the bar chart
-                layout = go.Layout(
-                    paper_bgcolor='rgba(0,0,0,0)',
-                    plot_bgcolor='rgba(0,0,0,0)',
-                    font=dict(family='sans serif', size=26, color='#ffffff')
-                )
-                #Create the chart, save as image, submit image, delete image
-                fig = go.Figure(data=[trace1], layout=layout)
-                py.image.save_as(fig, filename=(unCaller + '.png'))
-                await client.send_file(message.channel,(unCaller + '.png'))
-                await client.send_message(message.channel, "*Sit kid*")
-                os.remove((unCaller + '.png'))
-        except:
-            await client.send_message(message.author, "That skill does not exist")
-            return
+        #THIS WHERE YOU WORKING
+        proceed = False 
+        while (proceed = False):
+            try:
+                lvlCaller = dataCaller[labels.index(skill)].split(",")
+                proceed = True
+            except:
+                skill = await client.wait_for_message(timeout=15.0, author=message.author)
+                skill = skill.content.capitalize()
+        lvlCaller = dataCaller[labels.index(skill)].split(",") 
+        lvlRec = dataRec[labels.index(skill)].split(",")
+        if int(lvlCaller[1]) > int(lvlRec[1]):
+            await client.send_message(message.channel, 
+            "You ever show off your lvl.%d in %s just to flex on them %s niggas?\n**Flex Strength:** %d Levels %s XP" 
+            %(int(lvlCaller[1]),skill,unRec,(int(lvlCaller[1]) - int(lvlRec[1])),"{:,}".format((int(lvlCaller[2]) - int(lvlRec[2])))))
+            #Traces out the bar chart
+            trace1 = go.Bar(
+                x= [unCaller, unRec],
+                y= [int(lvlCaller[2]),int(lvlRec[2])],
+                text= ["{:,}".format(int(lvlCaller[2])),"{:,}".format(int(lvlRec[2]))],
+                textposition = 'auto',
+                marker=dict(color=['#16a085','#cb4335'])
+            )
+            #Layout for the bar chart
+            layout = go.Layout(
+                paper_bgcolor='rgba(0,0,0,0)',
+                plot_bgcolor='rgba(0,0,0,0)',
+                font=dict(family='sans serif', size=26, color='#ffffff')
+            )
+            #Create the chart, save as image, submit image, delete image
+            fig = go.Figure(data=[trace1], layout=layout)
+            py.image.save_as(fig, filename=(unCaller + '.png'))
+            await client.send_file(message.channel,(unCaller + '.png'))
+            await client.send_message(message.channel, "*Sit kid*")
+            os.remove((unCaller + '.png'))
         return
     #Gets the RuneScape stats for the input user and send them as a message
     elif message.content.startswith("$stats"):
