690e74c510a6e15fed1e59be7c0874914914a5b1
Nathan Fernandes <4nandes@gmail.com>
2018-07-04 19:33:29 -0700

    Modified ge to use embeds

--- cogs/GE.py
+++ cogs/GE.py
@@ -1,6 +1,9 @@
 from discord.ext import commands
+from discord import embeds
 import requests
 
+
+#add an option that will show all of the details for an item and otherwise just put what the 30 day trend is
 class GE:
     def __init__(self, bot):
         self.bot = bot
@@ -11,13 +14,21 @@ class GE:
         try:
             sauce = requests.get("http://services.runescape.com/m=itemdb_oldschool/api/catalogue/items.json?category=1&alpha={}".format(item)).json()
             sauce = requests.get("http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item={}".format(sauce['items'][0]['id'])).json()
-            msg = sauce['item']['name']  + '\n\n'
-            msg += "`Current Price:........  " + str(sauce['item']['current']['price']) + " gp`"
-            msg += "\n`Today's Trend:........  " + str(sauce['item']['today']['price']).replace(' ','') + " gp`"
-            msg += "\n`30 Day Trend:.........  " + str(sauce['item']['day30']['change']) + "`"
-            msg += "\n`90 Day Trend:.........  " + str(sauce['item']['day90']['change']) + "`"
-            msg += "\n`180 Day Trend:........  " + str(sauce['item']['day180']['change']) + "`"
-            await ctx.bot.say(msg)
+            msg = "**Current Price:**\n" + str(sauce['item']['current']['price']) + " gp\n"
+            msg += "**Today's Change:**\n" + str(sauce['item']['today']['price']).replace(' ','') + " gp\n"
+            #msg += "**30 Day Trend:**\n" + str(sauce['item']['day30']['change']) + "\n"
+            #msg += "**90 Day Trend:**\n" + str(sauce['item']['day90']['change']) + "\n"
+            #msg += "**180 Day Trend:**\n" + str(sauce['item']['day180']['change']) + "\n"
+            if str(sauce['item']['today']['price']).startswith("+"):
+                color = 0x2ecc71
+            elif str(sauce['item']['today']['price']).startswith("+"):
+                color = 0xe74c3c
+            else:
+                color = 0x3498db
+            emb = embeds.Embed(title=sauce['item']['name'], description=msg, color=color)
+            emb.set_thumbnail(url=sauce['item']['icon'])
+            emb.set_footer(text=sauce['item']['description'])
+            await ctx.bot.say(embed=emb)
         except:
             await ctx.bot.say(item + " not found")
             return
