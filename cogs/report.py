f4f04a0fa4e6edce2270246aad97ad78c64d5269
Nathan Fernandes <4nandes@gmail.com>
2018-07-20 03:10:13 -0700

    Day is now report so that it can have a modification where you put in the amount of time that you would like to go back into your date

--- cogs/report.py
+++ cogs/report.py
@@ -5,13 +5,13 @@ from lib.labels import labels
 from discord import embeds
 import datetime
 
-class day:
+class report:
     def __init__(self, bot):
         self.bot = bot
         self.labels = labels().getLabels()
 
     @commands.command(pass_context=True)
-    async def day(self,ctx):
+    async def report(self,ctx):
         username = database().searchDefault(ctx.message.author.id,ctx.message.server.id)
         if username == "":
             await ctx.bot.say("You must be registered to use this command")
@@ -28,8 +28,7 @@ class day:
         totXP = "{:,}".format(int(dataNow[0].split(",")[2]) - dataThen[0])
         emb.set_footer(text=(str(discordName) + " (" + totXP + "xp)"), icon_url=ctx.message.author.avatar_url)
         await ctx.bot.say(embed=emb)
-    
 
 def setup(bot):
-    bot.add_cog(day(bot))
+    bot.add_cog(report(bot))
     print("LOADED")
